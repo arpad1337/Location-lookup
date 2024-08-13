6df695121b85fde091b294651bc1320baac4232d
Kiss <arpad1337@me.com>
2024-08-13 10:55:05 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/*
+ * Location lookup
+ * v1.0
+ * By Arpad Kiss <arpad@greeneyes.ai>
+ */
+
+class DataLoader {}
+
+class Program {
+    public static main() {}
+}
+
+if (require.main) {
+    Program.main();
+} else {
+    module.exports = Program;
+}
